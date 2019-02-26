1d4bd2a2872077c5dc6b9c40106fae95c36a5a28
Kelly Online <Kellyonline2016@gmail.com>
2019-02-26 23:36:33 +0100

    Add c#

---
+++ C#/Array.cs
@@ -0,0 +1,28 @@
+using System;
+namespace MyCSharpProject
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Create an array of string type
+      string[] names = {"Jim","Kate","Sam","Sally"};
+      // Create an array of int type
+      int[] ages = {26,18,49,33};
+      // Display second item in names array (array indexing starts at 0)
+      Console.WriteLine(names[1]);
+      // Display fourth item in ages array
+      Console.WriteLine(ages[3]);
+      // Store the names array's length (number of elements in array) in variable
+      int arrayLength = names.Length;
+      // Display names array's length
+      Console.WriteLine(arrayLength);
+      // Go through each element in names array and check if Sam exists in array
+      for (int i = 0; i < arrayLength; i++) {
+        if (names[i] == "Sam") {
+          Console.WriteLine("Sam is here!");
+        }
+      }
+    }
+  }
+}
