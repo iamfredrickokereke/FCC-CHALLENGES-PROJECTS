dfb37c298e4a00f989a7d13304b1b6c252ceb948
Kelly Online <Kellyonline2016@gmail.com>
2019-02-26 23:37:42 +0100

    Add c#

--- C#/Array.cs
+++ C#/Array.cs
@@ -26,3 +26,47 @@ namespace MyCSharpProject
     }
   }
 }
+
+
+
+// test loop
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] arr = new string[4]; // Initialize.
+        arr[0] = "uno";               // First element.
+        arr[1] = "dos";               // Second.
+        arr[2] = "tres";              // Third.
+        arr[3] = "cuatro";            // Fourth.
+
+        // Loop over strings.
+        for (int i = 0; i < arr.Length; i++)
+        {
+            string s = arr[i];
+            Console.WriteLine(s);
+        }
+
+        // Loop over strings backwards.
+        for (int i = arr.Length - 1; i >= 0; i--)
+        {
+            string s = arr[i];
+            Console.WriteLine(s);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
