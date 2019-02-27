04435f29e595f7bd43f2068141a19bd13d26bf5f
Kelly Online <Kellyonline2016@gmail.com>
2019-02-27 23:45:36 +0100

    Add c#

---
+++ C#/ex2.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Example1 {
+  public static void example1 (string[] args) {
+
+    Console.Write("Enter your name: ");
+    string name = Console.ReadLine();
+    
+    Console.WriteLine("Hello " + name);
+
+  }
+}
